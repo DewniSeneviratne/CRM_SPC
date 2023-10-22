9a797b56462b97ae1064f52018ba15061c97e4ec
Chamali Wedage <94621140+ChamaliWedage-99@users.noreply.github.com>
2023-10-22 20:59:26 +0530

    Add files via upload

---
+++ spc/spc/staff.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace spc
+{
+    public partial class staff : Form
+    {
+        private DatabaseHelper dbHelper;
+        public staff()
+        {
+            InitializeComponent();
+            dbHelper = new DatabaseHelper();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            stock i = new stock();
+            i.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Delivery i = new Delivery();
+            i.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            this.Close();
+            Delivery i = new Delivery();
+            i.Show();
+        }
+     
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Prescriptions i = new Prescriptions();
+            i.Show();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            staff i = new staff();
+            i.Show();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Form1 i = new Form1();
+            i.Show();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            home i = new home();
+            i.Show();
+        }
+
+        private void staff_Load(object sender, EventArgs e)
+        {
+            LoadAuditLogData();
+        }
+        private void LoadAuditLogData()
+        {
+            using (var connection = dbHelper.CreateConnection())
+            {
+                connection.Open();
+
+                string query = "SELECT LogID, TableName, Operation, RecordID, Timestamp FROM AuditLog";
+
+                using (var dataAdapter = new SqlDataAdapter(query, connection))
+                {
+                    using (var dataTable = new DataTable())
+                    {
+                        dataAdapter.Fill(dataTable);
+
+                        dataGridView1.DataSource = dataTable;
+                    }
+                }
+            }
+        }
+    }
+}
