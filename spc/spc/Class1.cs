1b822c1270cc84c6278d1d29909fef482219a4cc
dewmi_20 <87789934+DNHathurusingha@users.noreply.github.com>
2023-10-22 21:14:06 +0530

    add class

---
+++ spc/spc/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.SqlClient;
+
+namespace spc
+{
+    internal class DatabaseHelper
+    {
+        private string connectionString;
+
+        public DatabaseHelper()
+        {
+            connectionString = "Server=DEWNI\\SQLEXPRESS;Database=CRMKotteDB;Integrated Security=True;";
+        }
+
+        public SqlConnection CreateConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
